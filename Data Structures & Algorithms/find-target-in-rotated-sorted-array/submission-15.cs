6b6aa85ff2dee5fa6130a6088075eb51ce2db27c
sinaeuroquip <sina@euroquip.co.nz>
2026-08-23 18:02:32 +0600

    Add: find-target-in-rotated-sorted-array - submission-15

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-15.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        var b = 0;
+var e = nums.Length - 1;
+while (b <= e)
+{
+    var m = b + (e - b) / 2;
+    if (nums[m] == target)
+        return m;
+
+    if (nums[b] <= nums[m])
+    {
+        if (nums[b] <= target && target <= nums[m])
+        {
+            e = m - 1;
+        }
+        else
+        {
+            b = m + 1;
+        }
+    }
+    else
+    {
+        if (nums[m] <= target && target <= nums[e])
+        {
+            b = m + 1;
+        }
+        else
+        {
+            e = m - 1;
+        }
+    }
+}
+return -1;
+    }
+}
